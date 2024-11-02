f2db0ac6928bdbb555188a972516f4716e337370
Connor Joyce <steelcity2003@gmail.com>
2024-11-02 14:18:37 -0500

    app change

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+    return render_template('index.html', output=None)
+
+@app.route('/calculate')
+def calculate():
+    amt_corn = request.form.get('amt_corn')
+    quality = request.form.get('quality')
+    destination = request.form.get('destiation')
+    cost = request.form.get('cost')
+    corn = amt_corn * (quality * .1)
+    dest = destination / 1000
+    if dest > 1:
+        output = (cost*corn) / dest
+    else:
+        output = (cost*corn) / 1
+    return render_template('index.html', output=output)
 
 if __name__ == '__main__':
 
